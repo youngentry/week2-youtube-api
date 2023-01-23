692e9add7586764a2937f7dd7e657cb98b032659
unknown <gentry_@naver.com>
2023-01-23 09:37:46 +0900

    feat: video page, autoplay when hovering mouse on a video thumbnail

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+import { useNavigate } from "react-router";
 import "../css/components/VideoCard.scss";
 
 const VideoCard = ({ mockVideo }) => {
-  console.log({ mockVideo });
+  const [playVideo, setPlayVideo] = useState(false);
+  const navigate = useNavigate();
   return (
-    <>
-      <li className="video-card">
+    <li onClick={() => navigate(`/video/${mockVideo.id}`, { state: { mockVideo } })} className="video-card">
+      <div className="thumbnailBox">
         <img src={`${mockVideo.snippet.thumbnails.medium.url}`} alt="" />
-        <div className="info">
-          <div className="picture"> </div>
-          <div className="text">
-            <p className="title">{mockVideo.snippet.title}</p>
-            <span className="name">{mockVideo.snippet.channelTitle}</span>
-            <div className="other">
-              <div className="view">{mockVideo.snippet.view}</div>
-              <div className="date">{mockVideo.snippet.publishedAt}</div>
-            </div>
+        <ReactPlayer
+          className="videoPlayer"
+          url={`https://www.youtube.com/embed/${mockVideo.id}&origin=http://localhost:3000/`}
+          muted={true}
+          playing={playVideo}
+          width="100%"
+          height="100%"
+          onMouseOver={() => setPlayVideo(true)}
+          onMouseLeave={() => setPlayVideo(false)}
+        />
+      </div>
+      <div className="info">
+        <div className="picture"> </div>
+        <div className="text">
+          <p className="title">{mockVideo.snippet.title}</p>
+          <span className="name">{mockVideo.snippet.channelTitle}</span>
+          <div className="other">
+            <div className="view">{mockVideo.snippet.view}</div>
+            <div className="date">{mockVideo.snippet.publishedAt}</div>
           </div>
         </div>
-      </li>
-    </>
+      </div>
+    </li>
   );
 };
 
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,11 +1,35 @@
 import React from "react";
+import { useLocation, useParams } from "react-router";
 import VideoCard from "../components/VideoCard";
 import "../css/pages/Video.scss";
 
 const Video = () => {
+  const {
+    state: { mockVideo },
+  } = useLocation();
+  console.log(mockVideo);
+
   return (
     <div className="video">
-      <ul>비디오</ul>
+      <iframe id="player" type="text/html" width="100%" height="80%" src={`http://www.youtube.com/embed/${mockVideo.id}`} />
+      <div className="info">
+        <div className="titleBox">
+          <strong className="title">{mockVideo.snippet.title}</strong>
+        </div>
+        <div className="infoBox">
+          <div className="channelInfoBox">
+            <div className="name">{mockVideo.snippet.channelTitle}</div>
+            <div className="subscribers">구독자수 1.11천명</div>
+          </div>
+          <div className="subscribe">
+            <button>구독</button>
+          </div>
+          <div className="activity">
+            <div className="like">👍좋아요</div>
+            <div className="dislike">👎싫어요</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
